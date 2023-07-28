a1eb0b9789127d4559fa37a6d3f34eb37a6bfc2a
Zard <ziyue.chen.apply@outlook.com>
2023-07-28 03:35:55 -0700

    End of section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import { Container, CssBaseline, createTheme, ThemeProvider} from "@mui/material";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode? 'dark' : 'light';
+  const theme = createTheme(
+    {
+      palette: {
+        mode: paletteType,
+        background: {
+          default: paletteType === 'light'? '#eaeaea' : '#121212'
+        }
+      }
+    }
+  )
+  
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <Container>
+        <Outlet />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+
---
+++ client/src/app/layout/Header.tsx
@@ -0,0 +1,66 @@
+import { ShoppingCart } from "@mui/icons-material";
+import { Badge, AppBar, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+interface Props {
+    darkMode: boolean;
+    handleThemeChange: () => void;
+}
+
+const midLinks = [
+    {title: 'catalog', path: '/catalog'},
+    {title: 'about', path: '/about'},
+    {title: 'catalog', path: '/contact'}
+]
+
+const rightLinks = [
+    {title: 'login', path: '/login'},
+    {title: 'register', path: '/about'}
+]
+
+export default function Header({darkMode, handleThemeChange}: Props) {
+    return (
+        <AppBar position="static" sx={{mb: 4}}>
+            <Toolbar>
+                <Typography variant='h6' component={NavLink}
+                    to='/'
+                    sx={{color: 'inherit', textDecoration: 'none'}}
+                >
+                    RE-store
+                </Typography>
+                <Switch checked={darkMode} onChange={handleThemeChange} color="secondary"/>
+                <List sx={{display: 'flex'}}>
+                    {midLinks.map(({title, path}) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={{color: 'inherit', typography: 'h6'}}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+
+                <IconButton size='large' edge='start' color='inherit' sx={{mr: 2}}>
+                    <Badge badgeContent='4' color='secondary'>
+                        <ShoppingCart />
+                    </Badge>
+                </IconButton>
+
+                <List sx={{display: 'flex'}}>
+                    {rightLinks.map(({title, path}) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={{color: 'inherit', typography: 'h6'}}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+            </Toolbar>
+        </AppBar>
+    )
+}
