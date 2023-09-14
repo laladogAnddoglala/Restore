f313b6dd4ac6f1acca4e57f5b43e0c1759a2205a
Zard <ziyue.chen.apply@outlook.com>
2023-09-13 22:52:16 -0700

    EF

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container, CssBaseline, createTheme, ThemeProvider, Typography} from "@mui/material";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
 
 export default function App() {
+  const {setBasket} = useStoreContext(); //Destructuring
+  const [loading, setLoading] = useState(true);
+
+  useEffect( () =>{
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
+    }
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode? 'dark' : 'light';
   const theme = createTheme(
